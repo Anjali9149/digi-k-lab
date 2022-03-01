857260ef21c7eb7f6e3e05a88f7f0730cf717c4b
Anjali9149 <100712568+Anjali9149@users.noreply.github.com>
2022-03-01 23:43:04 +0530

    Add files via upload

---
+++ collegejob/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Restaurants from './static/js/Restaurants';
+import './static/css/a.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Restaurants / >
+    </div>
+  );
+}
+
+export default App;
